81976ee86d5315f48306171100c0591cd8b4d05b
georgebakewell-smith <george.bakewell-smith@nottingham.ac.uk>
2023-10-03 22:54:39 +0100

    Add input.c, input.h, test linking

---
+++ src/input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "input.h"
+
+
+void helloWorld(){
+    printf("Hello World! This is a test\n");
+}
--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "input.h"
 
 int main(){
 
+    const unsigned int filePathLength = 60;
+    char *filePath = (char*)malloc(filePathLength*sizeof(char));
+
     printf("\nWelcome to Shortbread, your new directory cleanup tool!\n\n");
-    printf("Please enter an instruction:\n\n");
+    printf("Please enter your target directory:\n");
+    fgets(filePath, filePathLength, stdin);
+    printf("%s", filePath);
+    helloWorld();
+
+
+    free(filePath);
 
 return 0;
 }

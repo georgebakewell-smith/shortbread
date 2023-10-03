926e858bd8c6d8d19a0009ce4f0a471b3a7ca96f
georgebakewell-smith <george.bakewell-smith@nottingham.ac.uk>
2023-10-03 19:08:07 +0100

    Add Greeting

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(){
-printf("Hello World!\n");
+
+    printf("\nWelcome to Shortbread, your new directory cleanup tool!\n\n");
+    printf("Please enter an instruction:\n\n");
+
 return 0;
 }
 

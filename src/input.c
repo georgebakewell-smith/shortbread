d9925064c941c3acb015fd9313bc103fd466e1cc
georgebakewell-smith <george.bakewell-smith@nottingham.ac.uk>
2023-10-05 22:06:11 +0100

    Add LegInputs typedef structure in new leg_inputs.h

--- src/input.c
+++ src/input.c
@@ -2,45 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../include/tools.h"
+#include "../include/leg_inputs.h"
 
 int isLegit(char *userInput);
-void scanInput(char *userInput);
-
+size_t scanInput(char *userInput);
 
 void handleInput(){
 
     size_t inpLen = 60;
-    
+    size_t commandLen = 20;
     char *userInput = (char*)malloc(inpLen*sizeof(char));
-
-    fgets(userInput, inpLen, stdin);
+    char *command = (char*)malloc(commandLen*sizeof(char));
+    leginp *pLegInp = (struct LegInputs*)malloc(sizeof(leginp));
     
+
+    //Takes user input
+    fgets(userInput, inpLen, stdin);  
     newLineRemove(userInput);
 
-
-    printf("The command you entered is : %s", userInput);
+    //Checks command is legitimate
     if(isLegit(userInput)){
         printf("Legit!\n");
     }else{
         printf("Undefined command. Please try again.\n");
     }
-
+    
+    
+    free(pLegInp);
+    free(command);
     free(userInput);
 }
 
-void scanInput(char *userInput){
+
+size_t scanInput(char *userInput){
+    size_t tokenCount = 0;
 
     char delim[] = " ";
     char* token = strtok(userInput, delim);
 
     while (token != NULL) {
-        printf(" %s\n", token);
+        ++tokenCount;
+        printf("%s\n", token);
         token = strtok(NULL, delim);
     }
+    return tokenCount;
 }
 
-int isLegit(char *userInput){
-    scanInput(userInput);
+int isLegit(char *userInput){  
 
-    return 1;
+    if(scanInput(userInput) <= 2){
+        return 1;
+    }else{
+        return 0;
+    };
+
 }
